c5f622f84e567e9ea21c07a24bbaf86139166979
Whale77777 <kavyasathyanathan@gmail.com>
2023-11-14 17:28:06 +0530

    added

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -17,4 +17,21 @@ const GigCard = ({ item }) => {
             <img src="./img/star.png" alt="" />
             <span>{item.star}</span>
           </div>
-     
+        </div>
+        <hr />
+        <div className="detail">
+          <img src="./img/heart.png" alt="" />
+          <div className="price">
+            <span>STARTING AT</span>
+            <h2>
+              $ {item.price}
+              <sup>99</sup>
+            </h2>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default GigCard;
